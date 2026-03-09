ab6e216d4fcc95b701bfdd4dddde146ce66aea95
Shareonniey <sharonadhiambo015@gmail.com>
2026-03-09 07:22:07 +0300

    modifies the routes

--- server/app.py
+++ server/app.py
@@ -39,20 +39,33 @@ def get_heroes():
 def get_hero(id):
     hero = Hero.query.get(id)
     if hero:
-     return jsonify(hero.to_dict(include=['hero_powers'])), 200
+     return jsonify(hero.to_dict()), 200
     return jsonify({'error': 'Hero not found'}), 404
 
 @app.route('/powers', methods=['GET'])
 def get_powers():
     powers = Power.query.all()
-    return jsonify([power.to_dict() for power in powers], 200)
+    power_list = [
+       {
+          "id": power.id,
+          "name": power.name,
+          "description": power.description
+       }
+       for power in powers
+    ]
+    return jsonify(power_list), 200
 
 @app.route('/powers/<int:id>', methods=['GET'])
 def get_power(id):
     power = Power.query.get(id)
     if power:
-     return jsonify(power.to_dict()), 200
-    return jsonify({'error': 'Power not found'}), 404
+        return jsonify({
+       "id": power.id,
+       "name": power.name,
+       "description": power.description
+    }), 200
+
+    return jsonify({"error": "Power not found"}), 404
 
 @app.route('/powers/<int:id>', methods=['PATCH'])
 def update_power(id):
@@ -61,20 +74,27 @@ def update_power(id):
      return jsonify({'error': 'Power not found'}), 404
     
     data = request.get_json()
-    description = data.get('description')
-    if not description or description.strip() == '':
-       return jsonify({'errors': ['description cannot be empty']}), 400
     
-    power.description = description
-    db.session.commit()
-    return jsonify(power.to_dict()), 200
+    try:
+       power.description = data.get('description')
+       db.session.commit()
+       
+       return jsonify({
+          "id": power.id,
+          "name": power.name,
+          "description": power.description
+       }), 200
+    except Exception:
+        return jsonify({'errors': ['validation errors']}), 400
 
 @app.route('/hero_powers', methods=['POST'])
 def create_hero_power():
     data = request.get_json()
+
     hero_id = data.get('hero_id')
     power_id = data.get('power_id')
     strength = data.get('strength')
+    
     if not all([hero_id, power_id, strength]):
        return jsonify({'errors': ['All fields are required']}), 400
     hero = Hero.query.get(hero_id)
@@ -84,7 +104,14 @@ def create_hero_power():
     hero_power = HeroPower(hero_id=hero_id, power_id=power_id, strength=strength)
     db.session.add(hero_power)
     db.session.commit()
-    return jsonify(hero_power.to_dict(include=['hero', 'power'])), 201
+    return jsonify({
+       "id": hero_power.id,
+       "strength": hero_power.strength,
+       "hero_id": hero_power. hero_id,
+       "power_id": hero_power.power_id,
+       "hero": hero_power.hero.to_dict(),
+       "power": hero_power.power.to_dict()
+    }), 200
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
